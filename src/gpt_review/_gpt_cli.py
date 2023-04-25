dd15b77f3cda0553528d620d87af637bb0136c57
Daniel Ciborowski <dciborow@microsoft.com>
2023-04-25 18:48:43 -0400

    fix: Rename Package 2 (#4)

---
+++ src/gpt_review/_gpt_cli.py
@@ -0,0 +1,51 @@
+"""The GPT CLI configuration and utilities."""
+from collections import OrderedDict
+import os
+import sys
+
+from knack import CLI, CLICommandsLoader
+
+from gpt_review import __version__
+from gpt_review._ask import AskCommandGroup
+
+CLI_NAME = "gpt"
+
+
+class GPTCLI(CLI):
+    """Custom CLI implemntation to set version for the GPT CLI."""
+
+    def get_cli_version(self) -> str:
+        return __version__
+
+
+class GPTCommandsLoader(CLICommandsLoader):
+    """The GPT CLI Commands Loader."""
+
+    _CommandGroups = [
+        AskCommandGroup,
+    ]
+
+    _ArgumentGroups = [
+        AskCommandGroup,
+    ]
+
+    def load_command_table(self, args) -> OrderedDict:
+        for command_group in self._CommandGroups:
+            command_group.load_command_table(self)
+        return OrderedDict(self.command_table)
+
+    def load_arguments(self, command) -> None:
+        for argument_group in self._ArgumentGroups:
+            argument_group.load_arguments(self)
+        super(GPTCommandsLoader, self).load_arguments(command)
+
+
+def cli() -> int:
+    """The GPT CLI entry point."""
+    gpt = GPTCLI(
+        cli_name=CLI_NAME,
+        config_dir=os.path.expanduser(os.path.join("~", f".{CLI_NAME}")),
+        config_env_var_prefix=CLI_NAME,
+        commands_loader_cls=GPTCommandsLoader,
+    )
+    return gpt.invoke(sys.argv[1:])
---
+++ src/gpt_review/main.py
@@ -0,0 +1,21 @@
+"""The GPT CLI entry point."""
+import sys
+
+from knack.help_files import helps
+
+from gpt_review._gpt_cli import cli
+
+
+def _help_text(help_type, short_summary) -> str:
+    return f"""
+type: {help_type}
+short-summary: {short_summary}
+"""
+
+
+helps[""] = _help_text("group", "Easily interact with GPT APIs.")
+helps["git"] = _help_text("group", "Use GPT enchanced git commands.")
+
+
+exit_code = cli()
+sys.exit(exit_code)
