2fceeafd568b68fd296339d4eb5d2fe98d3aee0a
Daniel Ciborowski <dciborow@microsoft.com>
2023-04-25 17:08:02 +0000

    pull updates

--- tests/test_gpt_cli.py
+++ tests/test_gpt_cli.py
@@ -6,18 +6,23 @@ import sys
 
 from easy_gpt._gpt_cli import cli
 
-ARGS = [
+ROOT_COMMANDS = [
     "--version",
     "--help",
+]
+
+ASK_COMMANDS = [
     "ask --help",
     "ask how are you",
 ]
 
+ARGS = ROOT_COMMANDS + ASK_COMMANDS
+
 
 @pytest.mark.parametrize("command", ARGS)
-@pytest.mark.integration
-def test_int_gpt_cli(command):
-    """Test gpt --version"""
+@pytest.mark.cli
+def test_cli_gpt_cli(command):
+    """Test gpt commands from installed CLI"""
 
     command_array = f"gpt {command}".split(" ")
     result = subprocess.run(
@@ -39,6 +44,13 @@ def gpt_cli_test(command):
     else:
         exit_code = cli()
         assert exit_code == 0
+
+
+@pytest.mark.parametrize("command", ARGS)
+@pytest.mark.integration
+def test_int_gpt_cli(command):
+    """Test gpt commands from installed CLI"""
+    gpt_cli_test(command)
 
 
 @pytest.mark.parametrize(
