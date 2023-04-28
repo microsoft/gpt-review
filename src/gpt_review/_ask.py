86cc37d30a81059336fb15969a11f34ce5809694
Daniel Ciborowski <dciborow@microsoft.com>
2023-04-28 22:31:51 +0000

    fix how we load azure_context

--- src/gpt_review/_ask.py
+++ src/gpt_review/_ask.py
@@ -182,18 +182,17 @@ def _load_azure_openai_context() -> None:
 
     If the environment variables are not set, retrieve the values from Azure Key Vault.
     """
-    openai.api_type = "azure"
-    openai.api_version = "2023-03-15-preview"
+    os.environ["OPENAI_API_BASE"] = "azure"
     if os.getenv("AZURE_OPENAI_API"):
-        openai.api_base = os.getenv("AZURE_OPENAI_API")
-        openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+        os.environ["OPENAI_API_BASE"] = os.getenv("AZURE_OPENAI_API") # type: ignore
+        os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY") # type: ignore
     else:
         secret_client = SecretClient(
             vault_url=os.getenv("AZURE_KEY_VAULT_URL", DEFAULT_KEY_VAULT), credential=DefaultAzureCredential()
         )
 
-        openai.api_base = secret_client.get_secret("azure-open-ai").value
-        openai.api_key = secret_client.get_secret("azure-openai-key").value
+        os.environ["OPENAI_API_BASE"] = secret_client.get_secret("azure-open-ai").value # type: ignore
+        os.environ["OPENAI_API_KEY"] = secret_client.get_secret("azure-openai-key").value # type: ignore
 
 
 def _call_gpt(
