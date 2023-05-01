d9e5d9fdbbacf605848dd5a0c2426c36a716100e
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-01 03:12:38 -0400

    Update tests/conftest.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,6 +59,7 @@ def mock_github(monkeypatch) -> None:
 
     def mock_get(url, headers, timeout) -> MockResponse:
         return MockResponse()
+
     def mock_put(url, headers, timeout) -> MockResponse:
         return MockResponse()
 
