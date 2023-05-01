6e455222aa59eb0453df208ba129d46016a15601
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-01 16:37:59 +0000

    As an AI language model, I do not have a personal life as humans do. My purpose is to assist and provide information to users who interact with me.

--- src/gpt_review/_gpt_cli.py
+++ src/gpt_review/_gpt_cli.py
@@ -7,6 +7,7 @@ from knack import CLI, CLICommandsLoader
 
 from gpt_review import __version__
 from gpt_review._ask import AskCommandGroup
+from gpt_review._git import GitCommandGroup
 from gpt_review._github import GitHubCommandGroup
 
 CLI_NAME = "gpt"
@@ -25,6 +26,7 @@ class GPTCommandsLoader(CLICommandsLoader):
     _CommandGroups = [
         AskCommandGroup,
         GitHubCommandGroup,
+        GitCommandGroup,
     ]
 
     def load_command_table(self, args) -> OrderedDict:
