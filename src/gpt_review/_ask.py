1aabdbe639491ca7bf1cd85caf8bebbcf04466fb
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-08 17:16:40 -0400

    Apply suggestions from code review

--- src/gpt_review/_ask.py
+++ src/gpt_review/_ask.py
@@ -165,7 +165,7 @@ def _call_gpt(
         return completion.choices[0].message.content  # type: ignore
     except RateLimitError as error:
         if retry < C.MAX_RETRIES:
-            logging.warning("Call to GPT failed due to rate limit, retry attempt: %s", retry)
+            logging.warning("Call to GPT failed due to rate limit, retry attempt %s of %s", retry, C.RETRY_LIMIT)
             time.sleep(retry * 10)
             return _call_gpt(prompt, temperature, max_tokens, top_p, frequency_penalty, presence_penalty, retry + 1)
         raise RateLimitError("Retry limit exceeded") from error
