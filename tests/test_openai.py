509a35deccbfe2d492be78cba03a47bc75136261
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-08 23:09:59 +0000

    Add rate limit error handling to OpenAI wrapper tests.

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,6 +1,9 @@
 """Tests for the Open AI Wrapper."""
 import pytest
-from gpt_review._openai import _get_engine
+from openai.error import RateLimitError
+
+from gpt_review._openai import _call_gpt, _get_engine
+import gpt_review.constants as C
 
 
 def get_engine_test() -> None:
@@ -23,3 +26,21 @@ def test_get_engine() -> None:
 @pytest.mark.integration
 def test_int_get_engine() -> None:
     get_engine_test()
+
+
+def rate_limit_test(monkeypatch):
+    def mock_get_engine(prompt: str, max_tokens: int, fast: bool = False, large: bool = False):
+        raise RateLimitError("Rate Limit Error")
+
+    monkeypatch.setattr("gpt_review._openai._get_engine", mock_get_engine)
+    with pytest.raises(RateLimitError):
+        _call_gpt(prompt="This is a test prompt", retry=C.MAX_RETRIES)
+
+
+def test_rate_limit(monkeypatch) -> None:
+    rate_limit_test(monkeypatch)
+
+
+@pytest.mark.integration
+def test_int_rate_limit(monkeypatch) -> None:
+    rate_limit_test(monkeypatch)
