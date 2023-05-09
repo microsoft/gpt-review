9249eb2ab57148432c608536fee317d9b3e4486b
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 15:39:08 -0400

    Update suggestions.py

--- src/gpt_review/prompts/suggestions.py
+++ src/gpt_review/prompts/suggestions.py
@@ -4,23 +4,4 @@ from gpt_review.prompts.prompt import Prompt
 
 class SuggestionsPrompt(Prompt):
     """A prompt for the GPT Suggestion task."""
-
     prompt_suffix: str = "provide a list of suggestions of the code"
-
-
-class SuggestionsPrompt2(SuggestionsPrompt):
-    """An improved prompt for the GPT Suggestion task."""
-
-    system_message: str = "You are an experienced software developer."
-    examples: list = [
-        {
-            "role": "user",
-            "content": """
-""",
-        },
-        {
-            "role": "assistant",
-            "content": """
-""",
-        },
-    ]
