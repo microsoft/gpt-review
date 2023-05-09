d95aab35a5ec39c7ee1b5cd9fa7c4706e5c0dcad
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 18:30:06 -0400

    Update _coverage.py

--- src/gpt_review/prompts/_coverage.py
+++ src/gpt_review/prompts/_coverage.py
@@ -8,6 +8,7 @@ from gpt_review.prompts._prompt import Prompt
 class CoveragePrompt(Prompt):
     """A prompt for the GPT Coverage task."""
 
-    prompt_prefix: str = """You are an experienced software developer. Generate unit test cases for the code submitted
+    prompt_prefix: str = """Generate unit test cases for the code submitted
 in the pull request, ensuring comprehensive coverage of all functions, methods, and scenarios to validate the
 correctness and reliability of the implementation."""
+   system_message: str = "You are an experienced software developer."
