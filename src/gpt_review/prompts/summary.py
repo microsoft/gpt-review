919961a2fe359c379f6eecfd358f975e341441ee
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 15:41:48 -0400

    Update and rename review.py to summary.py

--- src/gpt_review/prompts/summary.py
+++ src/gpt_review/prompts/summary.py
@@ -2,8 +2,8 @@
 from gpt_review.prompts.prompt import Prompt
 
 
-class ReviewPrompt(Prompt):
-    """A prompt for the GPT review task."""
+class SummaryPrompt(Prompt):
+    """A prompt for the GPT summary task."""
 
     prompt_prefix: str = """Summarize the following file changed in a pull request submitted by a developer on GitHub,
 focusing on major modifications, additions, deletions, and any significant updates within the files. Do not include the
