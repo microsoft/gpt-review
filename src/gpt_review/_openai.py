1a2cd54933f3c42c4e03f284796697bf2a5bb846
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 18:05:03 -0400

    Apply suggestions from code review

--- src/gpt_review/_openai.py
+++ src/gpt_review/_openai.py
@@ -1,7 +1,3 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
 """Open AI API Call Wrapper."""
 import logging
 import time
