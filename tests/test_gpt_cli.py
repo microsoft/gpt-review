0e689105ca3c9d11494df579e5fd7754a4727f14
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 14:19:10 -0400

    Refactor test_gpt_cli.py and update error messages (#67)
    ## Description
    
    <!-- Prefix the PR title with 'feat:' to increment the minor version, or
    'fix:' to increment the patch version -->
    
    <!-- Describe what changes this PR introduces and their effects -->
    <!-- Update documentation if there are any changes to existing features
    or APIs -->
    
    ## Testing
    
    <!-- Describe how you tested your changes and how reviewers can test
    them -->
    <!-- Ensure all code can be tested offline (unmarked tests) and online
    (marked as 'integration') -->
    
    ## Additional context
    
    <!-- Add any other context about your changes here -->
    
    <!-- ## Breaking Changes -->
    
    <!-- List any breaking changes introduced by this PR and their
    implications and include '!' in the prefix. -->

--- tests/test_gpt_cli.py
+++ tests/test_gpt_cli.py
@@ -26,78 +26,76 @@ class CLICase2(CLICase):
     expected_error_code: int = 2
 
 
+SAMPLE_FILE = "src/gpt_review/__init__.py"
+
+QUESTION = "how are you"
+
+WHAT_LANGUAGE = "what programming language is this code written in?"
+
+HELP_TEXT = """usage: gpt ask [-h] [--verbose] [--debug] [--only-show-errors]
+               [--output {json,jsonc,yaml,yamlc,table,tsv,none}]
+               [--query JMESPATH] [--max-tokens MAX_TOKENS]
+               [--temperature TEMPERATURE] [--top-p TOP_P]
+               [--frequency-penalty FREQUENCY_PENALTY]
+               [--presence-penalty PRESENCE_PENALTY]
+               <QUESTION> [<QUESTION> ...]
+"""
+
 ROOT_COMMANDS = [
     CLICase("--version"),
     CLICase("--help"),
 ]
 
-WHAT_LANGUAGE = "what programming language is this code written in?"
-
 ASK_COMMANDS = [
     CLICase("ask --help"),
-    CLICase("ask how are you"),
-    CLICase("ask --fast how are you"),
+    CLICase(f"ask {QUESTION}"),
+    CLICase(f"ask --fast {QUESTION}"),
+    CLICase(
+        f"ask {QUESTION} --fast --max-tokens {C.MAX_TOKENS_DEFAULT} --temperature {C.TEMPERATURE_DEFAULT} --top-p {C.TOP_P_DEFAULT} --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.FREQUENCY_PENALTY_MAX}"
+    ),
+    CLICase1(
+        f"ask {QUESTION} --fast --max-tokens {C.MAX_TOKENS_MIN-1}",
+        f"ERROR: --max-tokens must be a(n) int between {C.MAX_TOKENS_MIN} and {C.MAX_TOKENS_MAX}\n",
+    ),
+    CLICase1(
+        f"ask {QUESTION} --temperature {C.TEMPERATURE_MAX+8}",
+        f"ERROR: --temperature must be a(n) float between {C.TEMPERATURE_MIN} and {C.TEMPERATURE_MAX}\n",
+    ),
+    CLICase1(
+        f"ask {QUESTION} --top-p {C.TOP_P_MAX+3.5}",
+        f"ERROR: --top-p must be a(n) float between {C.TOP_P_MIN} and {C.TOP_P_MAX}\n",
+    ),
+    CLICase1(
+        f"ask {QUESTION} --frequency-penalty {C.FREQUENCY_PENALTY_MAX+2}",
+        f"ERROR: --frequency-penalty must be a(n) float between {C.FREQUENCY_PENALTY_MIN} and {C.FREQUENCY_PENALTY_MAX}\n",
+    ),
+    CLICase1(
+        f"ask {QUESTION} --presence-penalty {C.PRESENCE_PENALTY_MAX+7.7}",
+        f"ERROR: --presence-penalty must be a(n) float between {C.PRESENCE_PENALTY_MIN} and {C.PRESENCE_PENALTY_MAX}\n",
+    ),
     CLICase2(
-        "ask how are you --fast --max-tokens",
-        """usage: gpt ask [-h] [--verbose] [--debug] [--only-show-errors]
-               [--output {json,jsonc,yaml,yamlc,table,tsv,none}]
-               [--query JMESPATH] [--max-tokens MAX_TOKENS]
-               [--temperature TEMPERATURE] [--top-p TOP_P]
-               [--frequency-penalty FREQUENCY_PENALTY]
-               [--presence-penalty PRESENCE_PENALTY]
-               <QUESTION> [<QUESTION> ...]
+        f"ask {QUESTION} --fast --max-tokens",
+        f"""{HELP_TEXT}
 gpt ask: error: argument --max-tokens: expected one argument
 """,
     ),
     CLICase2(
-        "ask how are you --fast --max-tokens 'test'",
-        """usage: gpt ask [-h] [--verbose] [--debug] [--only-show-errors]
-               [--output {json,jsonc,yaml,yamlc,table,tsv,none}]
-               [--query JMESPATH] [--max-tokens MAX_TOKENS]
-               [--temperature TEMPERATURE] [--top-p TOP_P]
-               [--frequency-penalty FREQUENCY_PENALTY]
-               [--presence-penalty PRESENCE_PENALTY]
-               <QUESTION> [<QUESTION> ...]
+        f"ask {QUESTION} --fast --max-tokens 'test'",
+        f"""{HELP_TEXT}
 gpt ask: error: argument --max-tokens: invalid int value: \"'test'\"
 """,
     ),
-    CLICase1(
-        f"ask how are you --fast --max-tokens {C.MAX_TOKENS_MIN-1}",
-        f"ERROR: --max-tokens must be a(n) int between {C.MAX_TOKENS_MIN} and {C.MAX_TOKENS_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --temperature {C.TEMPERATURE_MAX+8}",
-        f"ERROR: --temperature must be a(n) float between {C.TEMPERATURE_MIN} and {C.TEMPERATURE_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --top-p {C.TOP_P_MAX+3.5}",
-        f"ERROR: --top-p must be a(n) float between {C.TOP_P_MIN} and {C.TOP_P_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --frequency-penalty {C.FREQUENCY_PENALTY_MAX+2}",
-        f"ERROR: --frequency-penalty must be a(n) float between {C.FREQUENCY_PENALTY_MIN} and {C.FREQUENCY_PENALTY_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --presence-penalty {C.PRESENCE_PENALTY_MAX+7.7}",
-        f"ERROR: --presence-penalty must be a(n) float between {C.PRESENCE_PENALTY_MIN} and {C.PRESENCE_PENALTY_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --fast --max-tokens {C.MAX_TOKENS_MAX+1} --top-p {C.TOP_P_DEFAULT}",
-        f"ERROR: --max-tokens must be a(n) int between {C.MAX_TOKENS_MIN} and {C.MAX_TOKENS_MAX}\n",
-    ),
-    CLICase1(
-        f"ask how are you --fast --frequency-penalty {C.FREQUENCY_PENALTY_MAX+3} --presence-penalty {C.PRESENCE_PENALTY_MAX+7.7}",
-        f"ERROR: --frequency-penalty must be a(n) float between {C.FREQUENCY_PENALTY_MIN} and {C.FREQUENCY_PENALTY_MAX}\n",
-    ),
-    CLICase(
-        f"ask how are you --fast --max-tokens {C.MAX_TOKENS_DEFAULT} --temperature {C.TEMPERATURE_DEFAULT} --top-p {C.TOP_P_DEFAULT} --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.FREQUENCY_PENALTY_MAX}"
-    ),
-    CLICase("github review --help"),
-    CLICase("github review"),
+    CLICase(f"ask --files {SAMPLE_FILE} --files {SAMPLE_FILE} what programming language is this code written in?"),
+    CLICase(f"ask --fast -f {SAMPLE_FILE} what programming language is this code written in?"),
     CLICase(f"ask --files src/gpt_review/__init__.py --files src/gpt_review/__init__.py {WHAT_LANGUAGE}"),
     CLICase(f"ask --fast -f src/gpt_review/__init__.py {WHAT_LANGUAGE}"),
     CLICase(f"ask --fast -d src/gpt_review --recursive --hidden --required-exts .py {WHAT_LANGUAGE}"),
     CLICase(f"ask --fast -repo microsoft/gpt-review {WHAT_LANGUAGE}"),
+]
+
+GITHUB_COMMANDS = [
+    CLICase("github review --help"),
+    CLICase("github review"),
 ]
 
 GIT_COMMANDS = [
@@ -113,14 +111,21 @@ REVIEW_COMMANDS = [
     CLICase("review diff --diff tests/mock.diff --config tests/config.summary.test.yml"),
 ]
 
-ARGS = ROOT_COMMANDS + ASK_COMMANDS + GIT_COMMANDS + REVIEW_COMMANDS
+ARGS = ROOT_COMMANDS + ASK_COMMANDS + GIT_COMMANDS
+ARGS_DICT = {arg.command: arg for arg in ARGS}
+
+MODULE_COMMANDS = [
+    CLICase("python -m gpt --version"),
+    CLICase("python -m gpt_review --version"),
+]
+MODULE_DICT = {arg.command: arg for arg in MODULE_COMMANDS}
 
 
 def gpt_cli_test(command: CLICase) -> None:
     os.environ["GPT_ASK_COMMANDS"] = "1"
 
     sys.argv[1:] = command.command.split(" ")
-
+    exit_code = -1
     try:
         exit_code = cli()
     except SystemExit as e:
@@ -129,56 +134,42 @@ def gpt_cli_test(command: CLICase) -> None:
         assert exit_code == command.expected_error_code
 
 
-@pytest.mark.parametrize("command", ARGS)
-@pytest.mark.integration
-def test_int_gpt_cli(command: CLICase) -> None:
-    """Test gpt commands from CLI file"""
-    gpt_cli_test(command)
+def cli_test(command, command_array) -> None:
+    result = subprocess.run(
+        command_array,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False,
+    )
+
+    assert result.returncode == command.expected_error_code
 
 
-@pytest.mark.parametrize(
-    "command",
-    ARGS,
-)
-def test_gpt_cli(command: CLICase, mock_openai: None) -> None:
-    gpt_cli_test(command)
-
-
-@pytest.mark.parametrize("command", ARGS)
+@pytest.mark.parametrize("command", ARGS_DICT.keys())
 @pytest.mark.cli
-def test_cli_gpt_cli(command: CLICase) -> None:
+def test_cli_gpt_cli(command: str) -> None:
     """Test gpt commands from installed CLI"""
+    command_array = f"gpt {ARGS_DICT[command].command}".split(" ")
 
-    command_array = f"gpt {command.command}".split(" ")
-
-    result = subprocess.run(
-        command_array,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=False,
-    )
-
-    assert result.returncode == command.expected_error_code
+    cli_test(command, command_array)
 
 
-MODULE_COMMANDS = [
-    CLICase("python -m gpt --version"),
-    CLICase("python -m gpt_review --version"),
-]
-
-
-@pytest.mark.parametrize("command", MODULE_COMMANDS)
+@pytest.mark.parametrize("command", MODULE_DICT.keys())
 @pytest.mark.cli
-def test_cli_gpt_module(command: CLICase) -> None:
+def test_cli_gpt_module(command: str) -> None:
     """Test running cli as module"""
+    command_array = MODULE_DICT[command].command.split(" ")
 
-    command_array = command.command.split(" ")
+    cli_test(command, command_array)
 
-    result = subprocess.run(
-        command_array,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=False,
-    )
 
-    assert result.returncode == command.expected_error_code
+@pytest.mark.parametrize("command", ARGS_DICT.keys())
+def test_gpt_cli(command: str, mock_openai: None) -> None:
+    gpt_cli_test(ARGS_DICT[command])
+
+
+@pytest.mark.parametrize("command", ARGS_DICT.keys())
+@pytest.mark.integration
+def test_int_gpt_cli(command: str) -> None:
+    """Test gpt commands from CLI file"""
+    gpt_cli_test(ARGS_DICT[command])
