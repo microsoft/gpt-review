be2bd1034ae86bea42cca9cf7a1dc6dc1befdbd4
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-08 20:02:15 -0400

    Update test_gpt_cli.py

--- tests/test_gpt_cli.py
+++ tests/test_gpt_cli.py
@@ -16,6 +16,16 @@ class CLICase:
     expected_error_code: int = 0
 
 
+@dataclass
+class CLICase1(CLICase):
+    expected_error_code: int = 1
+
+
+@dataclass
+class CLICase2(CLICase):
+    expected_error_code: int = 2
+
+
 ROOT_COMMANDS = [
     CLICase("--version"),
     CLICase("--help"),
@@ -25,7 +35,7 @@ ASK_COMMANDS = [
     CLICase("ask --help"),
     CLICase("ask how are you"),
     CLICase("ask --fast how are you"),
-    CLICase(
+    CLICase2(
         "ask how are you --fast --max-tokens",
         """usage: gpt ask [-h] [--verbose] [--debug] [--only-show-errors]
                [--output {json,jsonc,yaml,yamlc,table,tsv,none}]
@@ -36,9 +46,8 @@ ASK_COMMANDS = [
                <QUESTION> [<QUESTION> ...]
 gpt ask: error: argument --max-tokens: expected one argument
 """,
-        2,
     ),
-    CLICase(
+    CLICase2(
         "ask how are you --fast --max-tokens 'test'",
         """usage: gpt ask [-h] [--verbose] [--debug] [--only-show-errors]
                [--output {json,jsonc,yaml,yamlc,table,tsv,none}]
@@ -49,51 +58,37 @@ gpt ask: error: argument --max-tokens: expected one argument
                <QUESTION> [<QUESTION> ...]
 gpt ask: error: argument --max-tokens: invalid int value: \"'test'\"
 """,
-        2,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --fast --max-tokens {C.MAX_TOKENS_MIN-1}",
         f"ERROR: --max-tokens must be a(n) int between {C.MAX_TOKENS_MIN} and {C.MAX_TOKENS_MAX}\n",
-        1,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --temperature {C.TEMPERATURE_MAX+8}",
         f"ERROR: --temperature must be a(n) float between {C.TEMPERATURE_MIN} and {C.TEMPERATURE_MAX}\n",
-        1,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --top-p {C.TOP_P_MAX+3.5}",
         f"ERROR: --top-p must be a(n) float between {C.TOP_P_MIN} and {C.TOP_P_MAX}\n",
-        1,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --frequency-penalty {C.FREQUENCY_PENALTY_MAX+2}",
         f"ERROR: --frequency-penalty must be a(n) float between {C.FREQUENCY_PENALTY_MIN} and {C.FREQUENCY_PENALTY_MAX}\n",
-        1,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --presence-penalty {C.PRESENCE_PENALTY_MAX+7.7}",
         f"ERROR: --presence-penalty must be a(n) float between {C.PRESENCE_PENALTY_MIN} and {C.PRESENCE_PENALTY_MAX}\n",
-        1,
     ),
-    CLICase(
+    CLICase1(
         f"ask how are you --fast --max-tokens {C.MAX_TOKENS_MAX+1} --top-p {C.TOP_P_DEFAULT}",
         f"ERROR: --max-tokens must be a(n) int between {C.MAX_TOKENS_MIN} and {C.MAX_TOKENS_MAX}\n",
-        1,
     ),
-    CLICase(
-        f"ask how are you --fast --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.PRESENCE_PENALTY_DEFAULT}"
-    ),
-    CLICase(
+    CLICase1(
         f"ask how are you --fast --frequency-penalty {C.FREQUENCY_PENALTY_MAX+3} --presence-penalty {C.PRESENCE_PENALTY_MAX+7.7}",
         f"ERROR: --frequency-penalty must be a(n) float between {C.FREQUENCY_PENALTY_MIN} and {C.FREQUENCY_PENALTY_MAX}\n",
-        1,
     ),
     CLICase(
-        f"ask how are you --fast --max-tokens={C.MAX_TOKENS_DEFAULT} --temperature {C.TEMPERATURE_DEFAULT} --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.FREQUENCY_PENALTY_MAX}"
-    ),
-    CLICase(
-        f"""ask how are you --fast --max-tokens {C.MAX_TOKENS_DEFAULT} --temperature {C.TEMPERATURE_DEFAULT} --top-p {C.TOP_P_DEFAULT} --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.FREQUENCY_PENALTY_MAX}"""
+        f"ask how are you --fast --max-tokens {C.MAX_TOKENS_DEFAULT} --temperature {C.TEMPERATURE_DEFAULT} --top-p {C.TOP_P_DEFAULT} --frequency-penalty {C.FREQUENCY_PENALTY_DEFAULT} --presence-penalty {C.FREQUENCY_PENALTY_MAX}"
     ),
     CLICase("github review --help"),
     CLICase("github review"),
