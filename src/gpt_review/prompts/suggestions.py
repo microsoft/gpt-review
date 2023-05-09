7b3399e05c28edd050a77b5552f3f91f205dd4b2
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-09 15:41:11 -0400

    Delete suggestions.py

--- src/gpt_review/prompts/suggestions.py
+++
@@ -1,7 +0,0 @@
-"""A prompt for the GPT Suggestion task."""
-from gpt_review.prompts.prompt import Prompt
-
-
-class SuggestionsPrompt(Prompt):
-    """A prompt for the GPT Suggestion task."""
-    prompt_suffix: str = "provide a list of suggestions of the code"
