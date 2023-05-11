aac46a7d75ae9df32295c29a862f6343a514b2e5
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-11 16:11:22 -0400

    Update _ask.py

--- src/gpt_review/_ask.py
+++ src/gpt_review/_ask.py
@@ -71,7 +71,7 @@ def _ask(
     fast: bool = False,
     large: bool = False,
     directory: Optional[str] = None,
-    refresh: bool = False,
+    reset: bool = False,
     required_exts: Optional[List[str]] = None,
     hidden: bool = False,
     recursive: bool = False,
@@ -95,6 +95,7 @@ def _ask(
         fast (bool, optional): Use the fast model. Defaults to False.
         large (bool, optional): Use the large model. Defaults to False.
         directory (Optional[str], optional): The directory to search. Defaults to None.
+        reset (bool, optional): Overwrite the existing storage directory.
         required_exts (Optional[List[str]], optional): The required file extensions. Defaults to None.
         hidden (bool, optional): Include hidden files. Defaults to False.
         recursive (bool, optional): Recursively search the directory. Defaults to False.
@@ -112,7 +113,7 @@ def _ask(
             prompt,
             files,
             input_dir=directory,
-            refresh=refresh,
+            reset=reset,
             exclude_hidden=not hidden,
             recursive=recursive,
             required_exts=required_exts,
@@ -264,8 +265,8 @@ class AskCommandGroup(GPTCommandGroup):
                 options_list=("--branch", "-b"),
             )
             args.argument(
-                "refresh",
-                help="Refresh the index. Requires --directory, --files, or --repository.",
+                "reset",
+                help="Rest the index by overwriting the storage directory. Requires --directory, --files, or --repository.",
                 default=False,
                 action="store_true",
             )
