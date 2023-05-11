dc4db6f974564c74b5087c3a52f4dd4f02e7a5cd
Daniel Ciborowski <dciborow@microsoft.com>
2023-05-11 16:11:38 -0400

    Update test_gpt_cli.py

--- tests/test_gpt_cli.py
+++ tests/test_gpt_cli.py
@@ -82,9 +82,9 @@ gpt ask: error: argument --max-tokens: expected one argument
 gpt ask: error: argument --max-tokens: invalid int value: \"'test'\"
 """,
     ),
-    CLICase(f"ask --files {SAMPLE_FILE} --files {SAMPLE_FILE} {WHAT_LANGUAGE} --refresh"),
+    CLICase(f"ask --files {SAMPLE_FILE} --files {SAMPLE_FILE} {WHAT_LANGUAGE} --reset"),
     CLICase(f"ask --fast -f {SAMPLE_FILE} {WHAT_LANGUAGE}"),
-    CLICase(f"ask --fast -d src/gpt_review --refresh --recursive --hidden --required-exts .py {WHAT_LANGUAGE}"),
+    CLICase(f"ask --fast -d src/gpt_review --reset --recursive --hidden --required-exts .py {WHAT_LANGUAGE}"),
     CLICase(f"ask --fast -repo microsoft/gpt-review --branch main {WHAT_LANGUAGE}"),
 ]
 
