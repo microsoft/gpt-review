c8661eeb1f47e6cb2f567a416e15a2370fb1696c
Danay <60160912+danay1999@users.noreply.github.com>
2023-06-15 16:13:21 -0400

    Delete __init__.py

--- src/gpt_review/__init__.py
+++
@@ -1,8 +0,0 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
-"""Easy GPT CLI"""
-from __future__ import annotations
-
-__version__ = "0.9.5"
